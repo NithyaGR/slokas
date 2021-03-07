b004aa452ab35037077edb1b1d69a4209ae1b5f6
NithyaGR <nithyakalyanigovindarajan@gmail.com>
2021-03-07 09:31:58 -0500

    api - return statement updated - removed localhost
    api changes

--- front-end/src/api/DataService.js
+++ front-end/src/api/DataService.js
@@ -3,41 +3,49 @@ import axios from 'axios';
 class myService {
     executeHomeService(){
         console.log('executed home service to get all books')
-        return axios.get('http://localhost:9000/book')
+        return axios.get('/book')
+        //return axios.get('http://localhost:9000/book')
         //return axios.get('http://localhost:9000/api/bgtext')
         
     }
     executeBookService(count){
         console.log('executed book service to get selected books- chapters' );
-        return axios.get(`http://localhost:9000/book/${count}`)
+        return axios.get(`/book/${count}`)
+        //return axios.get(`http://localhost:9000/book/${count}`)
     }
     executeBookServiceTexts(bookNo, chapterNo){
         console.log('executed book service to get texts from the selected chapter' );
-        return axios.get(`http://localhost:9000/book/${bookNo}/${chapterNo}`)
+        return axios.get(`/book/${bookNo}/${chapterNo}`)
+        //return axios.get(`http://localhost:9000/book/${bookNo}/${chapterNo}`)
     }
 
     executeUserServiceGetAll(){
         console.log('executed get all user service')
-        return axios.get('http://localhost:9000/users')     
+        return axios.get('/users')
+        //return axios.get('http://localhost:9000/users')     
     }
     executeUserServiceGetUser(email){
         console.log('executed get one user by email - service')
-        return axios.get(`http://localhost:9000/users/${email}`)     
+        return axios.get(`/users/${email}`)
+        //return axios.get(`http://localhost:9000/users/${email}`)     
     }
 
     executeUserServicePost(user){
         console.log('executed post user service')
-        return axios.post('http://localhost:9000/users', user)
+        return axios.post('/users', user)
+        //return axios.post('http://localhost:9000/users', user)
         
     }
 
     executeUserServiceUpdate(user){
         console.log('executed update/put user service')
-        return axios.put(`http://localhost:9000/users/${user.email}`, user)     
+        return axios.put(`/users/${user.email}`, user)
+        //return axios.put(`http://localhost:9000/users/${user.email}`, user)     
     }
     executeUserServiceDelete(email){
         console.log('executed delete user by email service')
-        return axios.delete(`http://localhost:9000/users/${email}`)
+        return axios.delete(`/users/${email}`)
+        //return axios.delete(`http://localhost:9000/users/${email}`)
     }
 
 }
