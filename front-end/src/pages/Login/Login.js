33827029c9e036a861b632020d5f2cbf329b9b42
NithyaGR <nithyakalyanigovindarajan@gmail.com>
2021-03-15 13:32:56 -0400

    Trying with bootstrap forms

--- front-end/src/pages/Login/Login.js
+++ front-end/src/pages/Login/Login.js
@@ -75,7 +75,54 @@ render(){
         <Container className="w-100 p-3">
         <div>
             <h3>Login</h3>
-            <div className='loginForm'>
+            <Form>
+                <Form.Group controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" id='inputEmail'/>
+                </Form.Group>
+
+                <Form.Group controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" id='inputPassword'/>
+                </Form.Group>
+                
+                <Button variant="primary" type="submit" onClick={this.handleClick}>
+                    Submit
+                </Button>
+                </Form>
+        </div>
+        </Container>
+  
+    
+    )
+}
+}
+export default Login;
+  
+/*
+<Form>
+  <Form.Group controlId="formBasicEmail">
+    <Form.Label>Email address</Form.Label>
+    <Form.Control type="email" placeholder="Enter email" />
+    <Form.Text className="text-muted">
+      We'll never share your email with anyone else.
+    </Form.Text>
+  </Form.Group>
+
+  <Form.Group controlId="formBasicPassword">
+    <Form.Label>Password</Form.Label>
+    <Form.Control type="password" placeholder="Password" />
+  </Form.Group>
+  <Form.Group controlId="formBasicCheckbox">
+    <Form.Check type="checkbox" label="Check me out" />
+  </Form.Group>
+  <Button variant="primary" type="submit">
+    Submit
+  </Button>
+</Form>
+*/
+/*
+<div className='loginForm'>
                 <br />
             <form>
                 <div className='form-group'>
@@ -91,12 +138,4 @@ render(){
                  <button type='submit' className='btn btn-primary' onClick={this.handleClick}>Submit</button>   
             </form>
             </div>   
-        </div>
-        </Container>
-  
-    
-    )
-}
-}
-export default Login;
-  
+*/
