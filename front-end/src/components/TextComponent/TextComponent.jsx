04478b45c1348242f4ba5ec71a2eeb917b9146a8
NithyaGR <nithyakalyanigovindarajan@gmail.com>
2021-03-28 19:56:20 -0400

    corrections - fonts changed - chapter1

--- front-end/src/components/TextComponent/TextComponent.jsx
+++ front-end/src/components/TextComponent/TextComponent.jsx
@@ -10,17 +10,23 @@ class TextComponent extends Component {
         console.log(this.props.display);
         return(
             <div className='text'>
+            <br/>
             <h4>{this.props.display.slokaNo}</h4>
+            <br/>
             <div className='sanskrit'>
+            <br/>
             <h4><pre>{this.props.display.textSanskrit}</pre></h4>
             </div>
+            <br/>
             <div className='english'>
             <pre>{this.props.display.textEnglish}</pre>
             {/* <h4>{this.props.display.textEnglish}</h4> */}
             </div>
             <div className='translation'>
+            <br/>
              <h5> Meaning </h5>   
-            <h6>{this.props.display.translation}</h6>
+             <br/>
+            <h5>{this.props.display.translation}</h5>
             </div>
             </div>
         )
