18ba540ddc2a4438d065a439c0ddfcaa4c6c89b8
NithyaGR <nithyakalyanigovindarajan@gmail.com>
2021-04-02 15:10:23 -0400

    css

--- front-end/src/components/NavigationBar/Navigation.js
+++ front-end/src/components/NavigationBar/Navigation.js
@@ -4,6 +4,7 @@ import { NavLink } from "react-router-dom";
 import { NavItem } from "react-bootstrap";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
+import styled from 'styled-components';
 
 class Navigation extends Component {
    
@@ -13,21 +14,30 @@ class Navigation extends Component {
     render(){
          console.log(sessionStorage.getItem('loggedInUser'));  
         return(
+            <Wrapper>
             <div>
                 <Navbar collapseOnSelect expand='lg' bg='dark' variant='dark' >
                 <Navbar.Brand href='/home'>Sri Krishna Asram</Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse className="justify-content-end" id="responsive-navbar-nav">
-                    <Nav.Link><NavLink to='/home' className="navlinks" style={{ textDecoration: 'none', color: 'white' }}>Home</NavLink></Nav.Link>
-                    <Nav.Link><NavLink to='/about' className="navlinks" style={{ textDecoration: 'none', color: 'white' }}>About</NavLink></Nav.Link>
-                    <Nav.Link><NavLink to='/register' className="navlinks" style={{ textDecoration: 'none', color: 'white' }}>Register</NavLink></Nav.Link>
+                    <Nav.Link><NavLink to='/home' className="navlinks" >Home</NavLink></Nav.Link>
+                    <Nav.Link><NavLink to='/about' className="navlinks" >About</NavLink></Nav.Link>
+                    <Nav.Link><NavLink to='/register' className="navlinks" >Register</NavLink></Nav.Link>
                     { this.props.userAuth 
-                    ?  <Nav.Link><NavLink to='/home' onClick={this.handleClick} className="navlinks"style={{ textDecoration: 'none', color: 'white' }}>Logout</NavLink></Nav.Link> 
-                    :  <Nav.Link><NavLink to='/login' className="navlinks" style={{ textDecoration: 'none', color: 'white' }}>Login</NavLink></Nav.Link> }
+                    ?  <Nav.Link><NavLink to='/home' onClick={this.handleClick} className="navlinks">Logout</NavLink></Nav.Link> 
+                    :  <Nav.Link><NavLink to='/login' className="navlinks" >Login</NavLink></Nav.Link> }
                 </Navbar.Collapse>
                 </Navbar>
             </div>
+            </Wrapper>
         );
 }
 }
+const Wrapper = styled.div `
+.navlinks{
+  color : white;
+  text-decoration: none;
+
+}
+`;
 export default Navigation;
